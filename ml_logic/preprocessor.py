a7f2e3a2fbe6556d913ee72e2959a1cd6bcb1410
Shailendra Veerasamy <shailendra.veerasamy@gmail.com>
2025-12-08 16:46:11 +0100

    Completed CatBoost with new data

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -1,6 +1,7 @@
 import joblib
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 # Where the scaler will be saved/loaded
 SCALER_PATH = Path("models/scaler.joblib")
@@ -47,3 +48,34 @@ def preprocess_for_inference(X: pd.DataFrame):
     X_scaled = scaler.transform(X)
 
     return X_scaled
+
+def preprocess_new_data(X: pd.DataFrame):
+    """
+    Apply the SAME preprocessing used during training.
+    In this project, scaling is the main preprocessing step.
+    """
+    X['dob'] = pd.to_datetime(X['dob'])
+    X['trans_date_trans_time'] = pd.to_datetime(X['trans_date_trans_time'])
+    X['age'] = (X['trans_date_trans_time'] - X['dob']).dt.days / 365.25
+    X['trans_month']=pd.to_datetime(X['trans_date_trans_time']).dt.month
+    X['trans_day']=pd.to_datetime(X['trans_date_trans_time']).dt.day
+    X['trans_hour']=pd.to_datetime(X['trans_date_trans_time']).dt.hour
+    X.drop(columns=['trans_date_trans_time','dob'],inplace=True)
+    X["trans_month_sin"] = np.sin(2 * np.pi * X["trans_month"] / 12)
+    X["trans_month_cos"] = np.cos(2 * np.pi * X["trans_month"] / 12)
+    X["trans_day_sin"] = np.sin(2 * np.pi * X["trans_day"] / 31)
+    X["trans_day_cos"] = np.cos(2 * np.pi * X["trans_day"] / 31)
+    X["trans_hour_sin"] = np.sin(2 * np.pi * X["trans_hour"] / 24)
+    X["trans_hour_cos"] = np.cos(2 * np.pi * X["trans_hour"] / 24)
+    X.drop(columns=['trans_month','trans_day','trans_hour'],inplace=True)
+
+    return X
+
+def drop_useless_columns(X: pd.DataFrame):
+    """
+    Drop useless columns
+    """
+    X.drop(columns=['Unnamed: 0','unix_time','cc_num','first', 'last', 'street', 'city', 'state', 'zip', 'city_pop', 'trans_num'],inplace=True)
+    X.drop(columns=['job','merchant'],inplace=True)
+
+    return X
